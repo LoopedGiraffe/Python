d9fd4a68a3f3e8a022a58f390f8c6362672f6b8a
LoopedGiraffe <loopedgiraffe@gmail.com>
2022-04-30 11:23:35 +0200

    Rollercoaster with options - many if statements
    many if statements and if / elif / else statement

---
+++ rollercoaster with picture (multiple if)/main.py
@@ -0,0 +1,26 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+bill = 0
+
+if height >= 120: 
+    print("Yoou can ride on rollercoaster")
+    age = int(input("What is your age?"))
+    if age < 12:
+        bill = 5
+        print("Child tickets are $5.")
+    elif age <= 18:
+        bill = 7
+        print("Youth tickets are $7.")
+    else:
+        bill = 9
+        print("Adult tickets are $9.")
+    
+    wants_photo = input("Do you want a photo taken? Y or N. ")
+    if wants_photo == "Y":
+        #add $3 to bill
+        bill = bill + 3
+        # shorter version: bill += 3
+    print(f"Your final bill is {bill}")
+
+else:
+    print("You need to grow some more")
