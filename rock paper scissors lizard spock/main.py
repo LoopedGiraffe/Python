8b27aa561fb25e6e409180cae7c95959f4c63607
LoopedGiraffe <loopedgiraffe@gmail.com>
2022-05-08 15:03:46 +0200

    Rock Scissors Paper Lizard Spock
    Fixed dealing with numers out of range

--- rock paper scissors lizard spock/main.py
+++ rock paper scissors lizard spock/main.py
@@ -46,44 +46,48 @@ spock = '''
  ___ _ __   ___   ___| | __
 / __| '_ \ / _ \ / __| |/ /
 \__ \ |_) | (_) | (__|   < 
-|___/ .__/ \___/ \___|_|\_\
+|___/ .__/ \___/ \___|_|\_|
     | |                    
     |_|                    
 '''
 
 import random
 chosen = int(input("\nPrint: \n0 for rock \n1 for paper \n2 for scissors \n3 for lizard \n4 for Spock \nWhat is your choice? \n"))
+computer_choice = random.randint(0, 2)
 
-if chosen == 0:
-    print(f"Your choice: \n {rock}")
-elif chosen == 1:
-    print(f"Your choice: \n {paper}")
-elif chosen == 2:
-    print(f"Your choice: \n {scissors}")
-elif chosen == 3:
-    print(f"Your choice: \n {lizard}")
-elif chosen == 4:
-    print(f"Your choice: \n {spock}")
+if chosen >= 0 and chosen <= 4:
+
+    if chosen == 0:
+        print(f"Your choice: \n {rock}")
+    elif chosen == 1:
+        print(f"Your choice: \n {paper}")
+    elif chosen == 2:
+        print(f"Your choice: \n {scissors}")
+    elif chosen == 3:
+        print(f"Your choice: \n {lizard}")
+    elif chosen == 4:
+        print(f"Your choice: \n {spock}")
+
+    if computer_choice == 0:
+        print(f"Computer choice: \n {rock}")
+    elif computer_choice == 1:
+        print(f"Computer choice: \n {paper}")
+    elif computer_choice == 2:
+        print(f"Computer choice: \n {scissors}")
+    elif computer_choice == 3:
+        print(f"Computer choice: \n {lizard}")
+    elif computer_choice == 4:
+        print(f"Computer choice: \n {spock}")
+
+    if chosen == computer_choice:
+        print("Same signs! \nNo one wins.")
+    elif (chosen == 0  and computer_choice == 2) or (chosen == 2 and computer_choice == 1) or (chosen == 1 and computer_choice == 0) or (chosen == 0  and computer_choice == 3) or (chosen == 3  and computer_choice == 4) or (chosen == 4  and computer_choice == 2) or (chosen == 2  and computer_choice == 3) or (chosen == 3  and computer_choice == 1) or (chosen == 1  and computer_choice == 4) or (chosen == 4  and computer_choice == 0):
+        print("Congratulations !!! \n You won!!!")
+    else:
+        print("sorry...\n You loose :(")
 else:
     print("Wrong options. Please choose: 0 or 1 or 2 or 3 or 4.")
 
 
-computer_choice = random.randint(0, 2)
 
-if computer_choice == 0:
-    print(f"Computer choice: \n {rock}")
-elif computer_choice == 1:
-    print(f"Computer choice: \n {paper}")
-elif computer_choice == 2:
-    print(f"Computer choice: \n {scissors}")
-elif computer_choice == 3:
-    print(f"Computer choice: \n {lizard}")
-elif computer_choice == 4:
-    print(f"Computer choice: \n {spock}")
 
-if chosen == computer_choice:
-    print("Same signs! \nNo one wins.")
-elif (chosen == 0  and computer_choice == 2) or (chosen == 2 and computer_choice == 1) or (chosen == 1 and computer_choice == 0) or (chosen == 0  and computer_choice == 3) or (chosen == 3  and computer_choice == 4) or (chosen == 4  and computer_choice == 2) or (chosen == 2  and computer_choice == 3) or (chosen == 3  and computer_choice == 1) or (chosen == 1  and computer_choice == 4) or (chosen == 4  and computer_choice == 0):
-    print("Congratulations !!! \n You won!!!")
-else:
-    print("sorry...\n You loose :(")
